9526fe0263b1dedc201fcd7898b290eedbf0ad83
Tirieru <tiriel@outlook.com>
2017-10-17 05:36:53 +0100

    database server files

---
+++ src/database/DatabaseMain.java
@@ -0,0 +1,10 @@
+package database;
+
+public class DatabaseMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/database/WideBoxDatabase.java
@@ -0,0 +1,35 @@
+package database;
+
+import java.util.Map;
+
+import common.Theater;
+
+
+public class WideBoxDatabase {
+	
+	private Map<Integer,String> getTeaters(){
+		
+	}
+	
+	
+	private Theater getTheaterInfo(int theaterId){
+		
+	}
+	
+	
+	private synchronized boolean reserveSeat(int clientId, int row, int column){
+		
+	}
+	
+	
+	private synchronized boolean acceptReservedSeat(int clientId){
+		
+	}
+	
+	
+	private synchronized boolean cancelReservation(int clientId){
+		
+	}
+	
+	
+}
