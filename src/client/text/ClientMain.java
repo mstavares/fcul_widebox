6d2c9b63427c453595b211a1c68435d226eb546d
Tirieru <tiriel@outlook.com>
2017-10-22 17:58:42 +0100

    first version of the text client, currently kinda sloopy

--- src/client/text/ClientMain.java
+++ src/client/text/ClientMain.java
@@ -1,10 +1,111 @@
 package client.text;
 
+import java.rmi.RemoteException;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Random;
+import java.util.Scanner;
+
+import client.WideBoxClient;
+import common.Theater;
+
 public class ClientMain {
 
+	public static final String ANSI_RESET = "\u001B[0m";
+	public static final String ANSI_RED = "\u001B[31m";
+	public static final String ANSI_GREEN = "\u001B[32m";
+	public static final String ANSI_YELLOW = "\u001B[33m";
+	public static final String ANSI_BLUE = "\u001B[34m";
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		Random rd = new Random();
+		String serverIp;
+		int serverPort;
+		int op;
+		int clientId = rd.nextInt(10000); //TODO get this from the properties file
+		
+		System.out.println("Insert the server IP:");
+		serverIp = sc.nextLine();
+		
+		System.out.println("Insert the server port:");
+		serverPort = sc.nextInt();
+		
+		try {
+			WideBoxClient client = new WideBoxClient(clientId, serverIp, serverPort);
+			Map<Integer,String> theaterList = client.getTheaters();
+			
+			System.out.println("Choose a theater: ");
+			for (Entry<Integer, String> t: theaterList.entrySet()){
+				System.out.println(t.getKey() + "- " + t.getValue());
+			}
+			
+			op = sc.nextInt();
+			
+			Theater theater = client.getTheaterInfo(op);
+			Boolean[][] seats = theater.getSeats();
+			boolean finished = false;
+			
+			while (!finished){
+				for (int i = 0; i < seats.length; i++){
+					for (int j = 0; j < seats [i].length; j++){
+						if (i == theater.getReservationRow() && j == theater.getReservationColumn())
+							System.out.print(ANSI_YELLOW + i + "." + j + " " + ANSI_RESET);
+						else if (seats[i][j])
+							System.out.print(ANSI_RED + i + "." + j + " " + ANSI_RESET);
+						else
+							System.out.print(i + j + " ");
+					}
+					System.out.print("\n");
+				}
+				System.out.println("The yellow seat is your current reservation, the red seats are occupied.");
+				System.out.println("Choose an option:");
+				System.out.println("1- Reserve a new seat.");
+				System.out.println("2- Accept reserved seat.");
+				System.out.println("3- Cancel reservation.");
+				System.out.println("0- Exit.");
+				
+				op = sc.nextInt();
+				
+				switch (op){
+					case 1:
+						System.out.println("Choose the row of the new seat:");
+						int row = sc.nextInt();
+						System.out.println("Choose the column of the new seat:");
+						int column = sc.nextInt();
+						
+						if ( client.reserveSeat(row, column) )
+							System.out.println("New seat reserved.");
+						else
+							System.out.println("Error reserving seat.");
+						break;
+						
+					case 2:
+						if ( client.acceptReservedSeat() ){
+							System.out.println("Seat accepted.");
+							finished = true;
+						}else
+							System.out.println("Error accepting seat.");
+						break;
+						
+					case 3:
+						if ( client.cancelReservation() ){
+							System.out.println("Reservation canceled.");
+							finished = true;
+						}else
+							System.out.println("Error canceling reservation.");
+						break;
+						
+					case 0:
+						finished = false;
+						break;
+				}
+			}
+		} catch (RemoteException e) {
+			System.out.println("Error connecting to the server.");
+		}
+		
+		sc.close();
 	}
-
+	
 }
--- src/common/Theater.java
+++ src/common/Theater.java
@@ -5,5 +5,44 @@ import java.io.Serializable;
 public class Theater implements Serializable{
 	
 	private static final long serialVersionUID = -3282175681599158763L;
-
+	
+	private Boolean[][] seats;
+	
+	private int clientId;
+	
+	private int reservationRow;
+	
+	private int reservationColumn;
+	
+	//TODO maybe a better way para guardar os dados aqui? ou nomes diferentes
+	
+	
+	public Theater(Boolean[][] seats, int clientId, int reservationRow, int reservationColumn) {
+		super();
+		this.seats = seats;
+		this.clientId = clientId;
+		this.reservationRow = reservationRow;
+		this.reservationColumn = reservationColumn;
+	}
+	
+	
+	public Boolean[][] getSeats() {
+		return seats;
+	}
+	
+	
+	public int getClientId() {
+		return clientId;
+	}
+	
+	
+	public int getReservationRow() {
+		return reservationRow;
+	}
+	
+	
+	public int getReservationColumn() {
+		return reservationColumn;
+	}
+	
 }
