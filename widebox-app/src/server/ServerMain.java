2551860ca83481d84bfcf28826fa5f3cc8558cb8
Tirieru <tiriel@outlook.com>
2017-10-27 15:42:14 +0100

    pede ip e porta no server caso não haja args

--- widebox-app/src/server/ServerMain.java
+++ widebox-app/src/server/ServerMain.java
@@ -2,11 +2,29 @@ package server;
 
 import java.io.IOException;
 import java.rmi.RemoteException;
+import java.util.Scanner;
 
 public class ServerMain {
 
 	public static void main(String[] args) throws IOException, RemoteException {
-		new WideBoxServerImpl(args[0], Integer.parseInt(args[1]));
+		String serverIp;
+		int serverPort;
+		Scanner sc = new Scanner(System.in);
+		
+		if (args.length >= 2){
+			serverIp = args[0];
+			serverPort = Integer.parseInt(args[1]);
+		}else{
+			System.out.println("Couldn't get ip and port arguments.");
+			System.out.println("Insert the server IP:");
+			serverIp = sc.nextLine();
+
+			System.out.println("Insert the server port:");
+			serverPort = sc.nextInt();
+		}
+		
+		sc.close();
+		new WideBoxServerImpl(serverIp, serverPort);
 	}
 
 }
